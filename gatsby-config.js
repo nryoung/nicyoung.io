6943276d4bfb826c60d39fe1770f9e8d7814ec69
Nic Young <nryoung@gmail.com>
2018-11-17 22:53:13 -0700

    chore: add stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
+        excerpt_separator: '<!-- end -->',
         plugins: [
           {
             resolve: 'gatsby-remark-images',
