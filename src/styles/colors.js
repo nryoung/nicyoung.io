eb5324fc02563006cbc80c3ed73c49853e270a7c
Nic Young <nryoung@gmail.com>
2018-11-18 10:40:31 -0700

    chore: remove more

--- src/styles/colors.js
+++
@@ -1,3 +0,0 @@
-export const white = '#fff';
-export const gray = '#999';
-export const black = '#000';
--- src/utils/cloudinary.js
+++
@@ -1,7 +0,0 @@
-const cloudinaryID = '1523222225';
-
-const getCloudinaryImage = (imageName, scale = 1920) => `
-  https://res.cloudinary.com/nicyoung/image/upload/c_scale,w_${scale}/v${cloudinaryID}/${imageName}.jpg
-`;
-
-export default getCloudinaryImage;
